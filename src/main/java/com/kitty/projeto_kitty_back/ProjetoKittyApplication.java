12382e779008bd7ed986eb59d9760c77e4c20c31
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 18:06:06 -0300

    alterando nome

--- src/main/java/com/kitty/projeto_kitty_back/ProjetoKittyApplication.java
+++ src/main/java/com/kitty/projeto_kitty_back/ProjetoKittyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoKittyBackApplication {
+public class ProjetoKittyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoKittyBackApplication.class, args);
+		SpringApplication.run(ProjetoKittyApplication.class, args);
 	}
 
 }
