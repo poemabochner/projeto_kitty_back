60e7f95d4f0e2c60e7ff2587d9df18211858d706
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 17:27:45 -0300

    criando projeto

---
+++ src/main/java/com/kitty/projeto_kitty_back/ProjetoKittyBackApplication.java
@@ -0,0 +1,13 @@
+package com.kitty.projeto_kitty_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoKittyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoKittyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitty/projeto_kitty_back/ProjetoKittyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitty.projeto_kitty_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoKittyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
