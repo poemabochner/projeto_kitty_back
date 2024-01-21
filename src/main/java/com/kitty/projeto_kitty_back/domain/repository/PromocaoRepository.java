af95b830306219ab0f2401f5ff404fb787efa138
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 21:03:04 -0300

    cria PromocaoRepository.java

---
+++ src/main/java/com/kitty/projeto_kitty_back/domain/repository/PromocaoRepository.java
@@ -0,0 +1,9 @@
+package com.kitty.projeto_kitty_back.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kitty.projeto_kitty_back.domain.model.Promocao;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+  
+}
