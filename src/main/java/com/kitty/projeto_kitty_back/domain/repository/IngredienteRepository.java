08c40d8a0bfeaf1c14d60963d497209e7daccc5e
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 22:03:00 -0300

    atualiza IngredienteRepository

--- src/main/java/com/kitty/projeto_kitty_back/domain/repository/IngredienteRepository.java
+++ src/main/java/com/kitty/projeto_kitty_back/domain/repository/IngredienteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kitty.projeto_kitty_back.domain.model.Ingrediente;
 
 public interface IngredienteRepository extends JpaRepository<Ingrediente, Long> {
-  
+  boolean existsByNomeIngrediente(String nomeIngrediente);
 }
