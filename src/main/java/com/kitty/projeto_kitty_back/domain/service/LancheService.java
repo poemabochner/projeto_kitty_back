9ff422d74129dda3fd67b77c2875e1b7b352e836
Poema Bochner <poemabochner8@gmail.com>
2024-01-21 13:10:55 -0300

    cria método cadastraLanche

--- src/main/java/com/kitty/projeto_kitty_back/domain/service/LancheService.java
+++ src/main/java/com/kitty/projeto_kitty_back/domain/service/LancheService.java
@@ -1,11 +1,14 @@
 package com.kitty.projeto_kitty_back.domain.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kitty.projeto_kitty_back.domain.model.Ingrediente;
 import com.kitty.projeto_kitty_back.domain.model.Lanche;
 import com.kitty.projeto_kitty_back.domain.repository.LancheRepository;
 
@@ -29,8 +32,26 @@ public class LancheService {
     return lancheEncontrado.get();
   }
 
+  public Lanche cadastraLanche(Lanche lanche) {
+    BigDecimal precoLanche = BigDecimal.ZERO;
+    List<Ingrediente> ingredientesLanche = new ArrayList<>();
+
+    for (Ingrediente ingrediente : lanche.getIngredientes()) {
+      ingredientesLanche.add(ingredienteService.buscaPorId(ingrediente.getIdIngrediente()));
+    }
+    lanche.setIngredientes(ingredientesLanche);
+
+    for (Ingrediente ingrediente : lanche.getIngredientes()) {
+      precoLanche = precoLanche.add(ingrediente.getPrecoIngrediente());
+    }
+    lanche.setPrecoLanche(precoLanche);
+
+    return lancheRepository.save(lanche);
+
+  }
+
   // public Lanche atualizaLanche(Long id) {
-  //   Lanche lancheExistente = getLancheById(id);
+  // Lanche lancheExistente = getLancheById(id);
   // }
 
   static class LancheException extends RuntimeException {
