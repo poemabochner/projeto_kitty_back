3c84a74a4e719b8791a1ccf3c541bb204756bb37
Poema Bochner <poemabochner8@gmail.com>
2024-01-21 15:58:14 -0300

    ajusta PromocaoRepository

--- src/main/java/com/kitty/projeto_kitty_back/domain/repository/PromocaoRepository.java
+++ src/main/java/com/kitty/projeto_kitty_back/domain/repository/PromocaoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kitty.projeto_kitty_back.domain.model.Promocao;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+  boolean existsByNomePromocao(String nomePromocao);
   
 }
