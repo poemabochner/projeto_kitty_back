8a038c0384e30d2e4f1e64e4bd910d87341fa3b0
Poema Bochner <poemabochner8@gmail.com>
2024-01-20 23:48:32 -0300

    IngredienteController

---
+++ src/main/java/com/kitty/projeto_kitty_back/controller/IngredienteController.java
@@ -0,0 +1,57 @@
+package com.kitty.projeto_kitty_back.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kitty.projeto_kitty_back.domain.model.Ingrediente;
+import com.kitty.projeto_kitty_back.domain.service.IngredienteService;
+
+@RestController
+@RequestMapping("/api/ingredientes")
+@CrossOrigin
+
+public class IngredienteController {
+  @Autowired
+  private IngredienteService ingredienteService;
+
+  @GetMapping
+    public List<Ingrediente> obterTodosIngredientes() {
+        return ingredienteService.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingrediente> buscaPorIdIngrediente(@PathVariable Long id) {
+        Ingrediente ingredienteEncontrado = ingredienteService.buscaPorId(id);
+        return ResponseEntity.ok(ingredienteEncontrado);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingrediente> criaIngrediente(@RequestBody Ingrediente ingrediente) {
+        Ingrediente ingredienteCriado = ingredienteService.criaIngrediente(ingrediente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ingredienteCriado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingrediente> atualizaIngrediente(@PathVariable Long id, @RequestBody Ingrediente ingrediente) {
+        Ingrediente ingredienteEditado = ingredienteService.atualizaIngrediente(id, ingrediente);
+        return ResponseEntity.ok(ingredienteEditado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletaIngrediente(@PathVariable Long id) {
+        ingredienteService.deletaIngrediente(id);
+        return ResponseEntity.noContent().build();
+    }
+}
